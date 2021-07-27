6df1d0f96d78d126bd3393db99d5d1a6d66a38fa
Basil <vogurt007@gmail.com>
2021-07-27 14:15:43 +0300

    обновление редми

--- api_bast/api/urls.py
+++ api_bast/api/urls.py
@@ -6,16 +6,11 @@ from rest_framework_simplejwt.views import (
 )
 from .views import UsersViewSet
 users_router = DefaultRouter()
-users_router.register('', UsersViewSet)
+users_router.register('users', UsersViewSet)
 
 
 urlpatterns = [
-    path('api/v1/users/', include(users_router.urls)),
-
-]
-
-
-urlpatterns += [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('', include(users_router.urls)),
 ]
